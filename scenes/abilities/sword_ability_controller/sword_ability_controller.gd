ca36108548048a9b8e37370b76a736ac5ae70815
Reece <reecemorgandev@gmail.com>
2024-07-11 11:54:52 +0100

    logic to make sword ability target enemies

--- scenes/abilities/sword_ability_controller/sword_ability_controller.gd
+++ scenes/abilities/sword_ability_controller/sword_ability_controller.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const MAX_RANGE = 150
+
 @export var sword_ability: PackedScene
 
 func _ready():
@@ -10,6 +12,20 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+	
+	if enemies.size() == 0:
+		return
+	
+	enemies.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+	
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
