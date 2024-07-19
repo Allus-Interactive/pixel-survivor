aecfde3461108631fa5d7d2cd97c23ff1f4898a7
Reece <reecemorgandev@gmail.com>
2024-07-19 15:59:58 +0100

    working enemy spawner

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,11 +1,20 @@
 extends Node
 
+const SPAWN_RADIUS = 375
 
-# Called when the node enters the scene tree for the first time.
+@export var basic_enemy_scene: PackedScene
+
 func _ready():
-	pass # Replace with function body.
+	$Timer.timeout.connect(on_timer_timeout)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	var random_direction = Vector2.RIGHT.rotated((randf_range(0, TAU)))
+	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+	
+	var enemy = basic_enemy_scene.instantiate() as Node2D
+	get_parent().add_child(enemy)
+	enemy.global_position = spawn_position
