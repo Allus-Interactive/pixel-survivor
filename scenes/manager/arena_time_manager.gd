9ed6a936c45edafd4917fcc0ee95700333c5db30
Reece <reecemorgandev@gmail.com>
2024-07-24 11:30:25 +0100

    created victory screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,6 +1,15 @@
 extends Node
 
+@export var victory_screen_sceen: PackedScene
+
 @onready var timer = $Timer
+
+func _ready():
+	timer.timeout.connect(on_timer_timeout)
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+func on_timer_timeout():
+	var victory_screen_instance = victory_screen_sceen.instantiate()
+	add_child(victory_screen_instance)
