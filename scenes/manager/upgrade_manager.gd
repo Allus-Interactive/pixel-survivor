ca95cbc8ffb40d89e826cd8ae3edd7e4c15386e1
Reece <reecemorgandev@gmail.com>
2024-07-24 09:59:07 +0100

    Update upgrade_manager.gd

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -28,7 +28,6 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
-	print(current_upgrades)
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	apply_upgrade(upgrade)
