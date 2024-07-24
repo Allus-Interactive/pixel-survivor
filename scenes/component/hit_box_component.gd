cf76197d2ebedd3bb5aaf11ac325380782807131
Reece <reecemorgandev@gmail.com>
2024-07-24 08:54:54 +0100

    updated damage logic to deal damage via ability

---
+++ scenes/component/hit_box_component.gd
@@ -0,0 +1,5 @@
+extends Area2D
+class_name HitBoxComponent
+
+var damage = 0
+
---
+++ scenes/component/hurt_box_component.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name hurtbox_component
+
+@export var health_component: Node
+
+func _ready():
+	area_entered.connect(on_area_entered)
+
+func on_area_entered(other_area: Area2D):
+	if not other_area is HitBoxComponent:
+		return
+	
+	if health_component == null:
+		return
+	
+	var hitbox_component = other_area as HitBoxComponent
+	health_component.damage(hitbox_component.damage)
