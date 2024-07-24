f767aa08b16f4ba745d2fe31dcc3508c21a5381a
Reece <reecemorgandev@gmail.com>
2024-07-24 10:58:13 +0100

    basic player health implemented

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,6 +3,15 @@ extends CharacterBody2D
 const ACCELERATION_SMOOTHING = 25
 const MAX_SPEED = 125
 
+@onready var damage_interval_timer = $DamageIntervalTimer
+
+var number_colliding_bodies = 0;
+
+func _ready():
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
@@ -15,5 +24,21 @@ func _process(delta):
 func get_movement_vector():	
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
 	return Vector2(x_movement, y_movement)
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	print($HealthComponent.current_health)
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
