0d1e4ab502b0ca0a9023a54722d0276f27d4dda1
Reece <reecemorgandev@gmail.com>
2024-07-28 09:24:12 +0100

    animated the player

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -7,6 +7,8 @@ const MAX_SPEED = 125
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
 @onready var abilities_manager = $AbilitiesManager
+@onready var animation_player = $AnimationPlayer
+@onready var visuals = $Visuals
 
 var number_colliding_bodies = 0;
 
@@ -26,6 +28,18 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	
 	move_and_slide()
+	
+	if movement_vector.x != 0 || movement_vector.y != 0:
+		animation_player.play("walk")
+	else:
+		animation_player.play("RESET")
+	
+	var move_sign = sign(movement_vector.x)
+	
+	if move_sign == 0:
+		visuals.scale = Vector2.ONE
+	else: 
+		visuals.scale = Vector2(move_sign, 1)
 
 func get_movement_vector():	
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
