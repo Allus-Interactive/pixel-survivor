9d439146599db1c87723883a0893d75d3d3aae6e
Reece <reecemorgandev@gmail.com>
2024-07-28 10:35:23 +0100

    refactored basic enemy

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -1,21 +1,12 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 40
-
-@onready var health_component: HealthComponent = $HealthComponent
 @onready var visuals = $Visuals
+@onready var velocity_component = $VelocityComponent
 
 func _process(delta):
-	var direction = get_direction_to_player()
-	velocity = direction * MAX_SPEED
-	move_and_slide()
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
 	
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(-move_sign, 1)
-
-func get_direction_to_player():
-	var player_node = get_tree().get_first_node_in_group("player") as Node2D
-	if player_node != null:
-		return (player_node.global_position - global_position).normalized()
-	return Vector2.ZERO
