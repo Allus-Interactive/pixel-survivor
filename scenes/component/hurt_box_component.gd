d3b7aaed0f088617a607bda8fea057b77881ddc2
Reece <reecemorgandev@gmail.com>
2024-07-29 09:35:22 +0100

    floating damage text

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -3,6 +3,8 @@ class_name hurtbox_component
 
 @export var health_component: Node
 
+var floating_text_scene = preload("res://scenes/UI/floating_text.tscn")
+
 func _ready():
 	area_entered.connect(on_area_entered)
 
@@ -15,3 +17,9 @@ func on_area_entered(other_area: Area2D):
 	
 	var hitbox_component = other_area as HitBoxComponent
 	health_component.damage(hitbox_component.damage)
+	
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
