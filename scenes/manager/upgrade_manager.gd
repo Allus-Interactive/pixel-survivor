8c3bb5b09ed87ef050243b18a738f19624ae04c4
Reece <reecemorgandev@gmail.com>
2024-07-29 10:59:56 +0100

    added new damage upgrades

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -1,12 +1,21 @@
 extends Node
 
-@export var upgrade_pool: Array[AbilityUpgrade]
 @export var exp_manager: Node
 @export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
+var upgrade_pool: WeightedTable = WeightedTable.new()
+
+var upgrade_sword_rate = preload("res://resources/upgrades/sword_rate.tres")
+var upgrade_sword_damage = preload("res://resources/upgrades/sword_damage.tres")
+var upgrade_axe = preload("res://resources/upgrades/axe.tres")
+var upgrade_axe_damage = preload("res://resources/upgrades/axe_damage.tres")
 
 func _ready():
+	upgrade_pool.add_item(upgrade_axe, 10)
+	upgrade_pool.add_item(upgrade_sword_rate, 10)
+	upgrade_pool.add_item(upgrade_sword_damage, 10)
+	
 	exp_manager.level_up.connect(on_level_up)
 
 func apply_upgrade(upgrade: AbilityUpgrade):
@@ -22,19 +31,22 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	if upgrade.max_quantity > 0:
 		var current_quantity = current_upgrades[upgrade.id]["quantity"]
 		if current_quantity == upgrade.max_quantity:
-			upgrade_pool = upgrade_pool.filter(func (pool_upgrade): return pool_upgrade.id != upgrade.id)
+			upgrade_pool.remove_item(upgrade)
 	
+	update_upgrade_pool(upgrade)
 	GameEvents.emit_ability_upgraded_added(upgrade, current_upgrades)
+
+func update_upgrade_pool(chosen_upgrade: AbilityUpgrade):
+	if chosen_upgrade.id == upgrade_axe.id:
+		upgrade_pool.add_item(upgrade_axe_damage, 10)
 
 func pick_upgrades():
 	var chosen_upgrades: Array[AbilityUpgrade] = []
-	var filtered_upgrades = upgrade_pool.duplicate()
-	#for i in 2:
-	for i in filtered_upgrades.size():
-		if filtered_upgrades.size() == 0:
+	
+	for i in 2:
+		if upgrade_pool.items.size() == chosen_upgrades.size():
 			break
-		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
-		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+		var chosen_upgrade = upgrade_pool.pick_item(chosen_upgrades)
 		chosen_upgrades.append(chosen_upgrade)
 	
 	return chosen_upgrades
