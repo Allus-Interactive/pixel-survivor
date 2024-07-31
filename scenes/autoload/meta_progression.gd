b36b570e2692277a9108e0a896168416032affe5
Reece <reecemorgandev@gmail.com>
2024-07-31 16:17:37 +0100

    save and load logic for meta progression

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SAVE_DATA_PATH = "user://game.save"
+
 var save_data: Dictionary = {
 	"win_count": 0,
 	"loss_count": 0,
@@ -9,6 +11,17 @@ var save_data: Dictionary = {
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_exp_collected)
+	load_save_data()
+
+func load_save_data():
+	if !FileAccess.file_exists(SAVE_DATA_PATH):
+		return
+	var file = FileAccess.open(SAVE_DATA_PATH, FileAccess.READ)
+	save_data = file.get_var()
+
+func save():
+	var file = FileAccess.open(SAVE_DATA_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
 	if !save_data["meta_upgrades"].has(upgrade.id):
@@ -19,5 +32,4 @@ func add_meta_upgrade(upgrade: MetaUpgrade):
 	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
 
 func on_exp_collected(number: float):
-	save_data["meta_upgrades_currency"] += number
-	pass
+	save_data["meta_upgrade_currency"] += number
